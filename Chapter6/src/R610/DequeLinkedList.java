742b3dbb90e788633a1d200c7ba727c15ac97d1f
Guilherme Stéfano <guilhermenostefano@gmail.com>
2023-05-01 16:42:38 -0300

    end chapter 6

---
+++ Chapter6/src/R610/DequeLinkedList.java
@@ -0,0 +1,50 @@
+package R610;
+
+import java.util.ArrayDeque;
+
+public class DequeLinkedList< E > implements IDeque < E > {
+
+	private ArrayDeque<E> deque = new ArrayDeque<E>();
+
+	@Override
+	public int size() {	
+		return deque.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return deque.isEmpty();
+	}
+
+	@Override
+	public E first() {	
+		return deque.getFirst();
+	}
+
+	@Override
+	public E last() {
+		return deque.getLast();
+	}
+
+	@Override
+	public void addFirst(E e) {
+		 deque.addFirst(e);
+	}
+
+	@Override
+	public void addLast(E e) {
+		 deque.addLast(e);
+	}
+
+	@Override
+	public E removeFirst() {
+		return deque.removeFirst();
+	}
+
+	@Override
+	public E removeLast() {
+		return deque.removeLast();
+	}
+	
+	
+}
